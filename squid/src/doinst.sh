1f67aa79dc2f90867a5a9d1a8c75da64a8bf5a18
Corrado Franco <conraid@linux.it>
2013-07-03 12:54:57 +0200

    Added squid and ulogd. Upgraded gmic. Rebuilt inkscape

---
+++ squid/src/doinst.sh
@@ -0,0 +1,31 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+perms etc/rc.d/rc.squid.new
+config etc/squid/mime.conf.new
+config etc/squid/squid.conf.new
+config etc/squid/cachemgr.conf.new
+config etc/squid/msntauth.conf.new
+config etc/logrotate.d/squid.new
+
