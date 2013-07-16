9b5a5b461750cb8dd447d0b9e68bff23b3016fb0
Corrado Franco <conraid@linux.it>
2013-07-16 09:38:53 +0200

    keepassx: fix SlackBuild

---
+++ keepassx/src/doinst.sh
@@ -0,0 +1,12 @@
+if [ -x /usr/bin/update-mime-database ]; then
+    /usr/bin/update-mime-database /usr/share/mime &> /dev/null
+fi
+  
+if [ -x /usr/bin/update-desktop-database ]; then
+    /usr/bin/update-desktop-database &> /dev/null
+fi
+
+if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache  -f -q /usr/share/icons/hicolor &> /dev/null
+fi
+      
