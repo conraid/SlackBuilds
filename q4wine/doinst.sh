e39545ac0bfebfc46b1d907391258bad6c8e2942
Corrado Franco <conraid@linux.it>
2014-06-25 16:27:05 +0200

    Upgraded p0f and wacomtablet. Deleted q4wine

--- q4wine/doinst.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Update the desktop database:
-if [ -x usr/bin/update-desktop-database ]; then
-  chroot . /usr/bin/update-desktop-database -q /usr/share/applications > /dev/null 2>&1
-fi
-
-
