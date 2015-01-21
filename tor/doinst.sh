8835b851a0e7a4b48fb4d2c37919e31ef87b90d6
Corrado Franco <conraid@linux.it>
2015-01-21 10:01:06 +0100

    tor: fix doinst.sh

--- tor/doinst.sh
+++ tor/doinst.sh
@@ -13,15 +13,21 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.tor.new:
-if [ -e etc/rc.d/rc.tor ]; then
-  cp -a etc/rc.d/rc.tor etc/rc.d/rc.tor.new.incoming
-  cat etc/rc.d/rc.tor.new > etc/rc.d/rc.tor.new.incoming
-  mv etc/rc.d/rc.tor.new.incoming etc/rc.d/rc.tor.new
-fi
+perms() {
+    # Keep same perms on file
+    NEW="$1"
+    OLD="$(dirname $NEW)/$(basename $NEW .new)"
+    if [ -e $OLD ]; then
+        cp -a $OLD $NEW.incoming
+        cat $NEW > $NEW.incoming
+        mv $NEW.incoming $NEW
+    fi
+    config $NEW
+}
 
-config etc/rc.d/rc.tor.new
+perms etc/rc.d/rc.tor.new
 config etc/tor/torrc.new
+config etc/logrotate.d/tor.new
 
 # Bail if user isn't valid on your system
 if ! grep -q "^tor:" etc/group ; then
