589360b5a54d7e561c33811d779a8fe585648311
Corrado Franco <conraid@linux.it>
2015-02-10 12:06:27 +0100

    p7zip: upgraded to 9.38

---
+++ p7zip/doinst.sh
@@ -0,0 +1,12 @@
+# Update the desktop database:
+if [ -x usr/bin/update-desktop-database ]; then
+  chroot . /usr/bin/update-desktop-database -q /usr/share/kde4/services/ServiceMenus > /dev/null 2>&1
+  chroot . /usr/bin/update-desktop-database -q /usr/share/applications > /dev/null 2>&1
+fi
+
+# Update icon cache if one exists
+if [ -r usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    chroot . /usr/bin/gtk-update-icon-cache -t -f -q usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
