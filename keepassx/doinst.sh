bd566e81732a71f7c0158a8d189be704cfd88988
Corrado Franco <conraid@linux.it>
2015-03-16 20:40:19 +0100

    keepassx: upgraded to 2.0_alpha6

--- keepassx/doinst.sh
+++ keepassx/doinst.sh
@@ -1,12 +1,16 @@
-if [ -x /usr/bin/update-mime-database ]; then
-    /usr/bin/update-mime-database /usr/share/mime &> /dev/null
-fi
-  
-if [ -x /usr/bin/update-desktop-database ]; then
-    /usr/bin/update-desktop-database &> /dev/null
+# Update the desktop database:
+if [ -x usr/bin/update-desktop-database ]; then
+	chroot . /usr/bin/update-desktop-database -q /usr/share/applications > /dev/null 2>&1
 fi
 
-if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache  -f -q /usr/share/icons/hicolor &> /dev/null
+# Update the mime database:
+if [ -x usr/bin/update-mime-database ]; then
+	chroot . /usr/bin/update-mime-database /usr/share/mime >/dev/null 2>&1
 fi
-      
+
+# Update icon cache if one exists
+if [ -r usr/share/icons/hicolor/icon-theme.cache ]; then
+	if [ -x /usr/bin/gtk-update-icon-cache ]; then
+		chroot . /usr/bin/gtk-update-icon-cache -t -f -q usr/share/icons/hicolor >/dev/null 2>&1
+	fi
+fi
