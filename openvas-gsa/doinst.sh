e80f975b36a813d910d52fa07ec839b50feb3105
Corrado Franco <conraid@linux.it>
2015-04-20 17:02:38 +0200

    Upgraded openvas packages

--- openvas-gsa/doinst.sh
+++ openvas-gsa/doinst.sh
@@ -12,19 +12,15 @@ config() {
 }
 
 perms() {
-  # Keep same perms on file
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
   if [ -e $OLD ]; then
-  cp -a $OLD $NEW.incoming
-  cat $NEW > $NEW.incoming
-  mv $NEW.incoming $NEW
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
   fi
   config $NEW
 }
 
-perms etc/rc.d/rc.openvasmd.new
-for NEW in etc/openvas/*.new; do
-  config $NEW
-done
-
+perms etc/rc.d/rc.gsad.new
+config etc/openvas/gsad_log.conf.new
--- openvas-scanner/doinst.sh
+++ openvas-scanner/doinst.sh
@@ -12,19 +12,14 @@ config() {
 }
 
 perms() {
-  # Keep same perms on file
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
   if [ -e $OLD ]; then
-  cp -a $OLD $NEW.incoming
-  cat $NEW > $NEW.incoming
-  mv $NEW.incoming $NEW
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
   fi
   config $NEW
 }
 
-perms etc/rc.d/rc.openvasmd.new
-for NEW in etc/openvas/*.new; do
-  config $NEW
-done
-
+perms etc/rc.d/rc.openvassd.new
