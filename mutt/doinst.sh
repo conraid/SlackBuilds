649507b4f4240c5d4c83f263c10b74c9bd156ba4
Corrado Franco <conraid@linux.it>
2015-09-01 12:08:07 +0200

    mutt: upgraded to 1.5.24. Added desktop file and icon. Fix sidebar patch

--- mutt/doinst.sh
+++ mutt/doinst.sh
@@ -12,3 +12,8 @@ config() {
 
 config etc/mutt/Muttrc.new
 config etc/mutt/mime.types.new
+
+# Update the desktop database:
+if [ -x usr/bin/update-desktop-database ]; then
+  chroot . /usr/bin/update-desktop-database -q /usr/share/applications > /dev/null 2>&1
+fi
