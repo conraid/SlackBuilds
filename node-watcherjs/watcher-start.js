cb1b2a159654fc752bfad3badff5c4401b850b82
Corrado Franco <conraid@linux.it>
2015-09-25 16:25:09 +0200

    node-watcherjs: added

---
+++ node-watcherjs/watcher-start.js
@@ -0,0 +1,3 @@
+// Source code to start watcher.js with default configuration
+var app = require('./src/watcher');
+app.watcherFactory.create().start();
---
+++ node-watcherjs/watcherjs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd /usr/lib%LIBDIRSUFFIX%/node_modules/watcher.js
+node ./watcher-start.js
