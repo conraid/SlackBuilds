eff86d6cf1c708d88baf3ef7a6b79a9f6336b9a4
Corrado Franco <conraid@linux.it>
2016-02-03 16:26:48 +0100

    atop: upgraded to 2.2-3

--- atop/doinst.sh
+++ atop/doinst.sh
@@ -16,9 +16,9 @@ perms() {
     NEW="$1"
     OLD="$(dirname $NEW)/$(basename $NEW .new)"
     if [ -e $OLD ]; then
-	cp -a $OLD $NEW.incoming
-	cat $NEW > $NEW.incoming
-	mv $NEW.incoming $NEW
+	   cp -a $OLD $NEW.incoming
+	   cat $NEW > $NEW.incoming
+	   mv $NEW.incoming $NEW
     fi
     config $NEW
 }
