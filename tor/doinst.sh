4077dba6db0f16e3b54ba5b792a49b3c89935698
Corrado Franco <conraid@linux.it>
2016-03-17 15:56:23 +0100

    tor: rebuilt

--- tor/doinst.sh
+++ tor/doinst.sh
@@ -12,15 +12,15 @@ config() {
 }
 
 perms() {
-    # Keep same perms on file
-    NEW="$1"
-    OLD="$(dirname $NEW)/$(basename $NEW .new)"
-    if [ -e $OLD ]; then
-        cp -a $OLD $NEW.incoming
-        cat $NEW > $NEW.incoming
-        mv $NEW.incoming $NEW
-    fi
-    config $NEW
+  # Keep same perms on file
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD $NEW.incoming
+    cat $NEW > $NEW.incoming
+    mv $NEW.incoming $NEW
+  fi
+  config $NEW
 }
 
 perms etc/rc.d/rc.tor.new
@@ -28,18 +28,17 @@ config etc/tor/torrc.new
 config etc/logrotate.d/tor.new
 
 # Add user and group (uid=220 and gid=220 are SBo suggest)
-if ! getent group tor 2>&1 > /dev/null; then
-    if ! getent group 220 2>&1 > /dev/null; then
-        chroot . groupadd -g 220 tor &>/dev/null
-    else
-        chroot . groupadd tor &>/dev/null
-    fi
+if ! grep -q "^tor:" etc/group; then
+  if ! grep -q ":220:" etc/group; then
+    chroot . groupadd -g 220 tor &>/dev/null
+  else
+    chroot . groupadd tor &>/dev/null
+  fi
 fi
-
-if ! getent passwd tor 2>&1 > /dev/null; then
-    if ! getent passwd 220 2>&1 > /dev/null; then
-        chroot . useradd -u 220 -d /dev/null -s /bin/false -c "The Onion Router" -g tor tor &>/dev/null
-    else
-        chroot . useradd -d /dev/null -s /bin/false -c "The Onion Router" -g tor tor &>/dev/null
-    fi
+if ! grep -q "^tor:" etc/passwd; then
+  if ! grep -q ":220:" etc/passwd; then
+    chroot . useradd -u 220 -d /dev/null -s /bin/false -c "The Onion Router" -g tor tor &>/dev/null
+  else
+    chroot . useradd -d /dev/null -s /bin/false -c "The Onion Router" -g tor tor &>/dev/null
+  fi
 fi
