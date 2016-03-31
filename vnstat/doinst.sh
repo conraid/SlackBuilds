7f5a2bd7e517cf312619accd9be53056614bee57
Corrado Franco <conraid@linux.it>
2016-03-31 15:46:19 +0200

    vnstat: upgraded to 1.15

--- vnstat/doinst.sh
+++ vnstat/doinst.sh
@@ -1,32 +1,28 @@
-#!/bin/sh
-
-perm() {
-    # Keep same perms on file
-    NEW="$1"
-    OLD="$(dirname $NEW)/$(basename $NEW .new)"
-    if [ -e $OLD ]; then
-	cp -a $OLD $NEW.incoming
-	cat $NEW > $NEW.incoming
-	mv $NEW.incoming $NEW
-    else 
-	chmod 0644 $NEW
-    fi
-}
-
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
-    mv $NEW $OLD
+  mv $NEW $OLD
   elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
-    rm $NEW
+  # toss the redundant copy
+  rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-perm etc/rc.d/rc.vnstatd.new
+perms() {
+  # Keep same perms on file
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+  cp -a $OLD $NEW.incoming
+  cat $NEW > $NEW.incoming
+  mv $NEW.incoming $NEW
+  fi
+  config $NEW
+}
+
+perms etc/rc.d/rc.vnstatd.new
 config etc/vnstat.conf.new
-config etc/rc.d/rc.vnstatd.new
 
