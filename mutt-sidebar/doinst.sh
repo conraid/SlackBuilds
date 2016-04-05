8ca302342e13f9a37643d73f7bb5a80c7508d780
Corrado Franco <conraid@linux.it>
2016-04-05 17:55:19 +0200

    mutt-sidebar: upgraded to 1.6.0

--- mutt-sidebar/doinst.sh
+++ mutt-sidebar/doinst.sh
@@ -1,13 +1,11 @@
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
-  # If there's no config file by that name, mv it over:
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
-    rm $NEW
+  mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+  rm $NEW
   fi
-  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
 config etc/mutt-sidebar/Muttrc.new
