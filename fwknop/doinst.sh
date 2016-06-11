27c1e554a8781867e4f0f80ea6b9b2be9311287e
Corrado Franco <conraid@linux.it>
2016-06-11 15:56:18 +0200

    fwknop: upgraded to 2.6.9

--- fwknop/doinst.sh
+++ fwknop/doinst.sh
@@ -12,28 +12,17 @@ config() {
 }
 
 perm() {
-    # Keep same perms on file
-    NEW="$1"
-    OLD="$(dirname $NEW)/$(basename $NEW .new)"
-    if [ -e $OLD ]; then
-	cp -a $OLD $NEW.incoming
-	cat $NEW > $NEW.incoming
+  # Keep same perms on file
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD $NEW.incoming
+    cat $NEW > $NEW.incoming
 	mv $NEW.incoming $NEW
-    fi
-    config $NEW
+  fi
+  config $NEW
 }
 
 perm etc/rc.d/rc.fwknopd.new
 config etc/fwknop/fwknopd.conf.new
 config etc/fwknop/access.conf.new
-
-echo "#"
-echo "# ****** IMPORTANT ******"
-echo "# fwknop-2.5"
-echo "#"
-echo "# If you are upgrading from an older version of fwknop,"
-echo "# you will want to read the \"Backwards Compatibility\" section of"
-echo "# the fwknop tutorial available here:"
-echo "#"
-echo "# cipherdyne.org/fwknop/docs/fwknop-tutorial.html#backwards-compatibility"
-echo "#"
