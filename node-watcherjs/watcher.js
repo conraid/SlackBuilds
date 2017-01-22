8e4a680b99c8e0e454e79fd551cf37e0d919fd76
Corrado Franco <conraid@linux.it>
2017-01-22 10:58:40 +0100

    Rebuilt node related packages

--- node-watcherjs/watcher.js
+++
@@ -1,3 +0,0 @@
-// Source code to start watcher.js with default configuration
-var app = require('/tmp/pkg/package-watcherjs/usr/lib64/node_modules/watcher.js/src/watcher');
-app.watcherFactory.create().start();
