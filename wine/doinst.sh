dfca6175a3b46391e2714116753de50e284f8369
Corrado Franco <conraid@linux.it>
2017-01-25 12:16:51 +0100

    wine: upgraded to 2.0

--- wine/doinst.sh
+++ wine/doinst.sh
@@ -1,6 +1,6 @@
 # Update the desktop database:
 if [ -x usr/bin/update-desktop-database ]; then
-	chroot . /usr/bin/update-desktop-database -q /usr/share/applications > /dev/null 2>&1
+  chroot . /usr/bin/update-desktop-database -q /usr/share/applications > /dev/null 2>&1
 fi
 
 
