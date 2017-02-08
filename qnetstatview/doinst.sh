0883e9e2999abc798e3ad9f3134ea012026bcac1
Corrado Franco <conraid@linux.it>
2017-02-08 10:25:02 +0100

    Fix doinst.sh in some packages

--- qnetstatview/doinst.sh
+++ qnetstatview/doinst.sh
@@ -1,4 +1,3 @@
-# Update the desktop database:
-if [ -x usr/bin/update-desktop-database ]; then
-  chroot . /usr/bin/update-desktop-database -q /usr/share/applications > /dev/null 2>&1
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications > /dev/null 2>&1
 fi
