663d8ae22bafdbd875ac0d478c62832b5edd5b36
Corrado Franco <conraid@gmail.com>
2019-01-28 18:31:44 +0100

    slpkg: upgraded to 3.4.1

--- slpkg/doinst.sh
+++ slpkg/doinst.sh
@@ -1,19 +1,17 @@
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
     rm $NEW
   fi
+  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-CONFIGS="slpkg.conf repositories.conf blacklist slackware-mirrors default-repositories \
-    custom-repositories rlworkman.deps pkg_security"
-for file in $CONFIGS; do
-    config etc/slpkg/${file}.new
+for NEW in etc/slpkg/*.new; do
+  config $NEW
 done
 
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
-fi
