a961d808b6774ff91cedf972f9bc5060ed872181
Corrado Franco <conraid@gmail.com>
2019-06-03 12:16:41 +0200

    john: upgraded to 1.9.0_jumbo_1

--- john/files/john-64.sh
+++ john/files/john-64.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
 JOHNDIR=/usr/libexec/john
-${JOHNDIR}/john $*
+
+if grep -q '^flags.* avx' /proc/cpuinfo && [ -x $JOHNDIR/john-avx ]; then
+  ${JOHNDIR}/john-avx $*
+elif grep -q '^flags.* oxp' /proc/cpuinfo && [ -x $JOHNDIR/john-oxp ] ; then
+  ${JOHNDIR}/john-oxp $*
+else
+  ${JOHNDIR}/john $*
+fi
 
--- john/files/john.sh
+++ john/files/john.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -e
 
 JOHNDIR=/usr/libexec/john
-if grep -q '^flags.* sse2' /proc/cpuinfo; then
-    ${JOHNDIR}/john-sse2 $*
-elif grep -q '^flags.* mmx' /proc/cpuinfo; then
-    ${JOHNDIR}/john-mmx $*
+
+if grep -q '^flags.* avx' /proc/cpuinfo && [ -x $JOHNDIR/john-avx ]; then
+  ${JOHNDIR}/john-avx $*
+elif grep -q '^flags.* oxp' /proc/cpuinfo && [ -x $JOHNDIR/john-oxp ] ; then
+  ${JOHNDIR}/john-oxp $*
+elif grep -q '^flags.* sse2' /proc/cpuinfo && [ -x $JOHNDIR/john-sse2 ] ; then
+  ${JOHNDIR}/john-sse2 $*
+elif grep -q '^flags.* mmx' /proc/cpuinfo && [ -x $JOHNDIR/john-mmx ]; then
+  ${JOHNDIR}/john-mmx $*
 else
-    ${JOHNDIR}/john $*
+  ${JOHNDIR}/john $*
 fi
 
