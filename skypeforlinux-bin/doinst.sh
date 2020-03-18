b44fcf499f4430b661c95a800aeecbe13bf6010b
Corrado Franco <conraid@gmail.com>
2020-03-18 12:08:54 +0100

    skypeforlinux-bin: upgraded to 8.58.0.93

--- skypeforlinux-bin/doinst.sh
+++ skypeforlinux-bin/doinst.sh
@@ -1,12 +1,4 @@
-# Update the desktop database:
-if [ -x usr/bin/update-desktop-database ]; then
-  chroot . /usr/bin/update-desktop-database -q /usr/share/applications > /dev/null 2>&1
-fi
+#!/bin/sh
 
-# Update icon cache if one exists
-if [ -r usr/share/icons/hicolor/icon-theme.cache ]; then
-  if [ -x /usr/bin/gtk-update-icon-cache ]; then
-  	chroot . /usr/bin/gtk-update-icon-cache -t -f -q usr/share/icons/hicolor >/dev/null 2>&1
-  fi
-fi
-
+VERSION=$(ar p skypeforlinux-64.deb control.tar.gz | tar zxO ./control | grep Version | awk '{print $2}' | cut -d- -f1)
+echo $VERSION
