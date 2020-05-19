14772a73323cb86b1fadca7c6a66aecee050ee32
Corrado Franco <conraid@gmail.com>
2020-05-19 16:10:44 +0200

    skypeforlinux-bin: upgraded to 8.60.0.76

--- skypeforlinux-bin/doinst.sh
+++ skypeforlinux-bin/doinst.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-VERSION=$(ar p skypeforlinux-64.deb control.tar.gz | tar zxO ./control | grep Version | awk '{print $2}' | cut -d- -f1)
+VERSION=$(ar p $CWD/${PRGNAM}-64.deb control.tar.gz | tar -Ozxf - ./control | awk '/^Version:/{print $NF}')
+
 echo $VERSION
