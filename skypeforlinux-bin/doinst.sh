c53c6d900ccd875a6180f2bcf5cad8c28a9bd61c
Corrado Franco <conraid@gmail.com>
2020-05-21 14:59:18 +0200

    Upgraded audacity, intel-microcode and protobuf. Rebuilt mosh, ostinato, protobuf-c and sykerforlinux-bin

--- skypeforlinux-bin/doinst.sh
+++ skypeforlinux-bin/doinst.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
 
-VERSION=$(ar p $CWD/${PRGNAM}-64.deb control.tar.gz | tar -Ozxf - ./control | awk '/^Version:/{print $NF}')
-
-echo $VERSION
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
