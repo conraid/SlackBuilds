17ebcdb258a06e3d944a730bc10ae98b6deaf011
Corrado Franco <conraid@gmail.com>
2021-03-04 10:19:13 +0100

    Upgraded dropbox-bin

--- dropbox-bin/doinst.sh
+++ dropbox-bin/doinst.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+# Update the desktop database:
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications > /dev/null 2>&1
+fi
 
-curl -I -L -s https://www.dropbox.com/download?plat=lnx.x86_64 | grep location | rev | cut -d- -f1 | cut -d. -f3- | rev
+# Update icon cache if one exists
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+  	/usr/bin/gtk-update-icon-cache -t -f -q usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
 
