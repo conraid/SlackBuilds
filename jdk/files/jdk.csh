fbbe7fefad811cc11475462f42b11d0bd6c43f47
Corrado Franco <conraid@gmail.com>
2022-08-10 08:23:43 +0200

    Removed jdk

--- jdk/files/jdk.csh
+++
@@ -1,3 +0,0 @@
-#!/bin/csh
-setenv JAVA_HOME /usr/lib/java
-setenv PATH ${PATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin
--- jdk/files/jdk.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export JAVA_HOME=/usr/lib/java
-export PATH="${PATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin"
