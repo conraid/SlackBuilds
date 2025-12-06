f96c38ea195d4a06c029dcef4d777578e8e211a1
Corrado Franco <conraid@gmail.com>
2025-12-06 20:07:37 +0100

    Added python-slpkg.

---
+++ python-slpkg/doinst.sh
@@ -0,0 +1,14 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+FILES="slpkg repositories blacklist rules"
+for file in $FILES; do
+  config etc/slpkg/$file.toml.new
+done
