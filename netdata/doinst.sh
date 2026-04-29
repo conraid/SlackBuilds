108056085598c42477a381e87f7e7e12efd312f8
Corrado Franco <conraid@gmail.com>
2026-04-29 23:31:49 +0200

    Rebuilt netdata.

--- netdata/doinst.sh
+++ netdata/doinst.sh
@@ -1,73 +1,77 @@
 config() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-    elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
+	NEW="$1"
+	OLD="$(dirname $NEW)/$(basename $NEW .new)"
+	# If there's no config file by that name, mv it over:
+	if [ ! -r $OLD ]; then
+		mv $NEW $OLD
+	elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+		# toss the redundant copy
+		rm $NEW
+	fi
+	# Otherwise, we leave the .new copy for the admin to consider...
 }
 
 perms() {
-  # Keep same perms on file
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  if [ -e $OLD ]; then
-    cp -a $OLD $NEW.incoming
-    cat $NEW > $NEW.incoming
-    mv $NEW.incoming $NEnetdataW
-  fi
-  config $NEW
+	# Keep same perms on file
+	NEW="$1"
+	OLD="$(dirname $NEW)/$(basename $NEW .new)"
+	if [ -e $OLD ]; then
+		cp -a $OLD $NEW.incoming
+		cat $NEW >$NEW.incoming
+		mv $NEW.incoming $NEW
+	fi
+	config $NEW
 }
 
 perms etc/rc.d/rc.netdata.new
-for NEW in $(find etc/netdata/ -name "*.new"); do
-  config $NEW
-done
-
-for NEW in $(find usr/lib/netdata -name "*.new"); do
-  config $NEW
+for NEW in $(find etc/netdata var/lib/netdata usr/lib*/netdata -name "*.new" 2>/dev/null); do
+	config $NEW
 done
 
 # Add user and group
 # (338 uid and gid are SBo suggests https://slackbuilds.org/uid_gid.txt)
 if ! grep -q "^netdata:" etc/group; then
-  if ! grep -q ":338:" etc/group; then
-    chroot . groupadd -g 338 netdata &>/dev/null
-  else
-    chroot . groupadd netdata &>/dev/null
-  fi
+	if ! grep -q ":338:" etc/group; then
+		chroot . groupadd -g 338 netdata &>/dev/null
+	else
+		chroot . groupadd netdata &>/dev/null
+	fi
 fi
 
 if ! grep -q "^netdata:" etc/passwd; then
-  if ! grep -q ":338:" etc/passwd; then
-    chroot . useradd -u 338 -d /var/cache/netdata -s /bin/false -g netdata netdata &>/dev/null
-  else
-    chroot . useradd -d /var/cache/netdata -s /bin/false -g netdata netdata &>/dev/null
-  fi
+	if ! grep -q ":338:" etc/passwd; then
+		chroot . useradd -u 338 -d /var/cache/netdata -s /bin/false -g netdata netdata &>/dev/null
+	else
+		chroot . useradd -d /var/cache/netdata -s /bin/false -g netdata netdata &>/dev/null
+	fi
 fi
 
-if [ -x /usr/libexec/netdata/plugins.d/apps.plugin ] ; then
-setcap cap_dac_read_search,cap_sys_ptrace+ep "usr/libexec/netdata/plugins.d/apps.plugin"
-fi
-if [ -x /usr/libexec/netdata/plugins.d/debugfs.plugin ] ; then
-setcap cap_dac_read_search+ep "usr/libexec/netdata/plugins.d/debugfs.plugin"
-fi
-if [ -x /usr/libexec/netdata/plugins.d/go.d.plugin ] ; then
-setcap cap_dac_read_search,cap_net_admin,cap_net_raw+eip "usr/libexec/netdata/plugins.d/go.d.plugin"
-fi
-if [ -x /usr/libexec/netdata/plugins.d/logs-management.plugin ] ; then
-setcap cap_dac_read_search,cap_syslog+ep "usr/libexec/netdata/plugins.d/logs-management.plugin"
-fi
-if [ -x /usr/libexec/netdata/plugins.d/network-viewer.plugin ] ; then
-setcap cap_sys_admin,cap_sys_ptrace,cap_dac_read_search+ep "usr/libexec/netdata/plugins.d/network-viewer.plugin"
-fi
-if [ -x /usr/libexec/netdata/plugins.d/perd.plugin ] ; then
-setcap cap_perfmon,cap_sys_admin+ep "usr/libexec/netdata/plugins.d/perf.plugin"
-fi
-if [ -x /usr/libexec/netdata/plugins.d/slabinfo.plugin ] ; then
-setcap cap_dac_read_search+ep "usr/libexec/netdata/plugins.d/slabinfo.plugin"
-fi
+apply_caps() {
+	local FILE="usr/libexec/netdata/plugins.d/$1"
+	shift
+	local CAPS_LIST="$@"
+
+	if [ -x "$FILE" ]; then
+		for CAP in $CAPS_LIST; do
+			# Proviamo ad applicare la capability singolarmente
+			setcap "${CAP}+ep" "$FILE" 2>/dev/null
+		done
+	fi
+}
+
+apply_caps "ebpf.plugin" cap_sys_admin cap_sys_ptrace cap_dac_read_search cap_perfmon
+
+apply_caps "apps.plugin" cap_dac_read_search cap_sys_ptrace
+
+apply_caps "freeipmi.plugin" cap_sys_rawio
+
+apply_caps "network-viewer.plugin" cap_sys_admin cap_sys_ptrace cap_dac_read_search
+apply_caps "go.d.plugin" cap_net_admin cap_net_raw
+apply_caps "nfacct.plugin" cap_net_admin
+
+apply_caps "debugfs.plugin" cap_dac_read_search
+apply_caps "perf.plugin" cap_perfmon cap_sys_admin
+apply_caps "slabinfo.plugin" cap_dac_read_search
+apply_caps "ioping.plugin" cap_net_raw
+
+apply_caps "cups.plugin" cap_dac_override
